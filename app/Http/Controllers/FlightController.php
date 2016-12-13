d4515d1ddcdc0796ccf1b5316aca9b647236b379
Mauro <maurostehle@gmail.com>
2016-12-13 07:04:26 +0100

    Create Controller

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -4,16 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Flight;
+use App\lib;
 
 class FlightController extends Controller
 {
     public function flightList(){
+        $adapter = new FlightAwareJsonAdapter(FLIGHT_AWARE_NAME, FLIGHT_AWARE_KEY);
+        $flight = $adapter ->getFlight($identCode);
+        //$flight = $this->getTestFlight();
+        $cityDescription = $this->getWikiText($flight);
+        $cityPictures = $this->getDetailViewCityPictures($flight);
+
+        $this->view('flight/flightdetailview', ['flight' => $flight, 'cityDescription' => $cityDescription, 'cityPictures' => $cityPictures]);
         return view('flightListView', []);
 
     }
 
     public function flight($ident){
-        return view('flightDetailView', [$ident]);
 
+
+        return view('flightDetailView', [$ident]);
     }
 }
