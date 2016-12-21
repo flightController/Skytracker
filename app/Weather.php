bc5339dd577771c2c5350215e8cdf3f073c9b5d0
Patrick <patrick@elsasser.me>
2016-12-21 16:22:36 +0100

    Weather Class updated

--- app/Weather.php
+++ app/Weather.php
@@ -19,6 +19,64 @@ private $cloud;
 private $wind;
 private $icon;
 
+    /**
+     * Weather constructor.
+     * @param $temperature
+     * @param $weathercondition
+     * @param $cloud
+     * @param $wind
+     * @param $icon
+     */
+    public function __construct($temperature, $weathercondition, $cloud, $wind, $icon)
+    {
+        $this->temperature = $temperature;
+        $this->weathercondition = $weathercondition;
+        $this->cloud = $cloud;
+        $this->wind = $wind;
+        $this->icon = $icon;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTemperature()
+    {
+        return $this->temperature;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWeathercondition()
+    {
+        return $this->weathercondition;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCloud()
+    {
+        return $this->cloud;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWind()
+    {
+        return $this->wind;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIcon()
+    {
+        return $this->icon;
+    }
+
+
 
 
 }
