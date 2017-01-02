ef99bcd9c0fc84bb6ad78f865cb0ea888d8ad958
Mauro <maurostehle@gmail.com>
2017-01-02 16:27:55 +0100

    database migratios

---
+++ database/migrations/2016_12_17_123458_user_settings.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserSettings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('number_of_flights');
+            $table->integer('refresh_time');
+            $table->boolean('test_mode');
+            $table->string('home_airport');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
