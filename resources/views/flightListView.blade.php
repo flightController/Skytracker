5b2fe5cc6d80aa3e821e8be38dab91d017789a53
Humbel <humbi1992@gmail.com>
2017-01-25 16:30:25 +0100

    CSS Anpassungen Designe und Bildcrop

--- resources/views/flightListView.blade.php
+++ resources/views/flightListView.blade.php
@@ -21,7 +21,7 @@
             <div class="col-md-12 divider"></div>
             <div class="col-md-4 col-sm-4 weather"><img src="../images/icon/{{$weather[$flight->getDestination()->getLocation()] -> getIcon()}}.png" class="weathericon" width="40px" height="40px"> {{$weather[$flight->getDestination()->getLocation()] -> getWeatherCondition()}}
             </div>
-            <div class="col-md-4 col-sm-4 weather"><img src="../images/thermometer.ico" class="weathericon" width="20px" height="20px"> {{$weather[$flight->getDestination()->getLocation()] -> getTemperature()}} °C
+            <div class="col-md-4 col-sm-4 weather"><img src="../images/thermometer.ico" class="weathericon" width="20px" height="20px"> {{round($weather[$flight->getDestination()->getLocation()] -> getTemperature(),1)}} °C
             </div>
 
             <div class="col-md-4 moreinfobutton"><a href="/flight/{{$flight -> getIdent()}}"><button type="button" class="btn btn-primary btn-block"> Mehr Infos </button></a>
