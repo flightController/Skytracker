3b53b6f2310af0763989e20837f72390370f415e
andy <andreas.reimann.sui@gmail.com>
2017-01-31 10:54:27 +0100

    Sprachpaket Deutsch - noch nicht fertig

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Passwörter müssen mindestens sechs Zeichen lang sein und mit der Bestätigung übereinstimmen.',
+    'reset' => 'Dein Passwort wurde zurück gesetzt!',
+    'sent' => 'Wir haben den Link zum Zurücksetzen Ihres Passworts an Ihre E-Mail versendet',
+    'token' => 'Dieser Kennwort zurücksetzen Token ist ungültig.',
+    'user' => "Wir können keinen Benutzer mit dieser E-Mail-Adresse finden.",
+
+];
